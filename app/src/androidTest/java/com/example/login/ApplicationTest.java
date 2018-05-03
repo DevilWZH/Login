a9e8ac93ca285a519f990b12dd90226608d4ad60
DevilWZH <36835572+DevilWZH@users.noreply.github.com>
2018-05-03 15:00:08 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/login/ApplicationTest.java
@@ -0,0 +1,19 @@
+package com.example.login;
+
+/**
+ * Created by 王梓豪 on 2017/12/5.
+ */
+
+
+
+        import android.app.Application;
+        import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
